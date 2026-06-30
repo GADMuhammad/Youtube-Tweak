1f758086db32ae949d1273ec32bbeccc137c4f97
Muhammad Gad <muhammadgadwork@gmail.com>
2026-06-30 18:30:05 +0300

    translations

--- src/helpers/translationObject.js
+++ src/helpers/translationObject.js
@@ -24,11 +24,12 @@ export const toastText = {
   en: {
     loading: "Sorting videos by date...",
     success: "Subscriptions page sorted successfully!",
-    error: "Sorry, an error occurred while sorting."
+    error:
+      "Sorry, an error occurred while sorting. Try to Refresh the page again."
   },
   ar: {
     loading: "...جاري إعادة ترتيب الفيديوهات حسب التاريخ",
     success: "!تم ترتيب صفحة الاشتراكات بنجاح",
-    error: ".عذراً، حدث خطأ أثناء الترتيب"
+    error: "عذرًا حدث خطأ أثناء الترتيب. أعد تحميل الصفحة مرة أخري."
   }
 }
