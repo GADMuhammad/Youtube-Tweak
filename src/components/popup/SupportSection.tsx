3b2df6878b65778ef27ceeeb1b3fe1285d768d7d
Muhammad Gad <muhammadgadwork@gmail.com>
2026-07-07 19:44:05 +0300

    remove label

--- src/components/popup/SupportSection.tsx
+++ src/components/popup/SupportSection.tsx
@@ -15,19 +15,16 @@ const text = supportSectionText[isArabic ? "ar" : "en"]
 const CONTACT_LINKS = [
   {
     label: "LinkedIn",
-    value: "linkedin.com/in/mohgad",
     href: "https://www.linkedin.com/in/mohgad/",
     icon: <LinkedInIcon />
   },
   {
     label: "Telegram",
-    value: "@mohgad",
     href: "https://t.me/mohgad",
     icon: <TelegramIcon />
   },
   {
     label: "Email",
-    value: "gad.software.engineer@gmail.com",
     href: "mailto:gad.software.engineer@gmail.com",
     icon: <MailIcon />
   }
@@ -36,13 +33,11 @@ const CONTACT_LINKS = [
 const DONATION_LINKS = [
   {
     label: "Vodafone Cash",
-    value: "+201069429820",
     href: "http://vf.eg/vfcash?id=mt&qrId=yV2w6L",
     icon: <WalletIcon />
   },
   {
     label: "InstaPay",
-    value: "@mohgad_",
     href: "https://ipn.eg/S/mohgad_/instapay/1F8tgu",
     icon: <InstaPayIcon />
   }
@@ -50,12 +45,11 @@ const DONATION_LINKS = [
 
 interface LinkRowProps {
   label: string
-  value: string
   href: string
   icon: React.ReactNode
 }
 
-function LinkRow({ label, value, href, icon }: LinkRowProps) {
+function LinkRow({ label, href, icon }: LinkRowProps) {
   return (
     <a
       className="popup-support-link"
@@ -63,12 +57,7 @@ function LinkRow({ label, value, href, icon }: LinkRowProps) {
       target="_blank"
       rel="noopener noreferrer">
       <span className="popup-support-link__icon">{icon}</span>
-      <span className="popup-support-link__text">
-        <span className="popup-support-link__label">{label}</span>
-        <span className="popup-support-link__value" dir="ltr">
-          {value}
-        </span>
-      </span>
+      <span className="popup-support-link__label">{label}</span>
     </a>
   )
 }
