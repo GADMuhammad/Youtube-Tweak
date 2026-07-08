49b20b3ed91a2c951f6923f19d12d27de0e50842
Muhammad Gad <muhammadgadwork@gmail.com>
2026-07-09 00:29:36 +0300

    again

--- src/hooks/useYoutubeThemeAndDom.ts
+++ src/hooks/useYoutubeThemeAndDom.ts
@@ -6,12 +6,6 @@ export default function useYoutubeThemeAndDom() {
   )
 
   useEffect(() => {
-    // Hide 'latest' H2 to replace it with filter buttons:
-    const latestTextH2 = document.querySelector(
-      "h2.style-scope.ytd-shelf-renderer"
-    ) as HTMLElement
-    if (latestTextH2) latestTextH2.style.display = "none"
-
     // targeting the outside host element
     const allPlasmoCsuis = document.querySelectorAll("plasmo-csui")
 
@@ -22,7 +16,7 @@ export default function useYoutubeThemeAndDom() {
         ) as HTMLElement
 
         if (shadowContainer) {
-          // بنصفر الـ z-index لكل المكونات علشان نحمي الـ Layout بتاع يوتيوب
+          // to protect youtube layout
           shadowContainer.style.setProperty("z-index", "10", "important")
         }
       }
